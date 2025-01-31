71a30380dd2e648063b0862da6575ae4722821cb
Dhiego Cassiano Fogaça Barbosa <modscleo4@outlook.com>
2025-01-31 02:08:35 -0300

    Adição de cabeçalhos de licença Apache 2.0

--- DProxyClient/Program.cs
+++ DProxyClient/Program.cs
@@ -1,4 +1,20 @@
-﻿using Microsoft.IdentityModel.JsonWebTokens;
+﻿/**
+ * Copyright 2025 Dhiego Cassiano Fogaça Barbosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Microsoft.IdentityModel.JsonWebTokens;
 using System;
 using System.Buffers.Binary;
 using System.ComponentModel;
@@ -485,7 +501,7 @@ namespace DProxyClient
                             break;
                         }
 
-                        case DProxyPacketType.HANDSHAKE_RESPONSE:
+                        case DProxyPacketType.HEARTBEAT_RESPONSE:
                             var heartbeatResponse = await ReadHeartbeatResponse(stream, incomingHeader);
                             break;
 
